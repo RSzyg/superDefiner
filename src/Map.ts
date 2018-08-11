d57d4e3790a30aa8d4fd34fd711c1ad0231b0d98
gongrongyun <1716175849@qq.com>
2018-08-11 14:30:43 +0800

    Map

--- src/Map.ts
+++ src/Map.ts
@@ -3,35 +3,64 @@ import Shape from "./Shape";
 
 export default class Map extends Container {
     public static mainmap: number[][];
-    public static rect: Shape[];
+    public static rectLeft: Shape[];
+    public static rectRight: Shape[];
+    public static rectMid: Shape[];
+    public static line: Shape[];
     constructor() {
         super();
-        Map.rect = [];
-        for (let i = 0; i < 16; i++) {
-            Map.rect[i] = new Shape();
+        Map.rectLeft = [];
+        Map.rectRight = [];
+        Map.rectMid = [];
+        Map.line = [];
+        for (let i = 0; i < 64; i++) {
+            Map.rectLeft[i] = new Shape();
+        }
+        for (let i = 0; i < 64; i++) {
+            Map.rectRight[i] = new Shape();
+        }
+        for (let i = 0; i < 5; i++) {
+            Map.rectMid[i] = new Shape();
+        }
+        for (let i = 0; i < 47; i++) {
+            Map.line[i] = new Shape();
         }
     }
 
     public createMap() {
-        Map.rect[0].saveRect(80, 720, 40, 40);
-        Map.rect[1].saveRect(120, 720, 40, 40);
-        Map.rect[2].saveRect(80, 760, 40, 40);
-        Map.rect[3].saveRect(120, 760, 40, 40);
-        Map.rect[4].saveRect(840, 720, 40, 40);
-        Map.rect[5].saveRect(880, 720, 40, 40);
-        Map.rect[6].saveRect(840, 760, 40, 40);
-        Map.rect[7].saveRect(880, 760, 40, 40);
-        Map.rect[8].saveRect(200, 720, 40, 40);
-        Map.rect[9].saveRect(280, 720, 40, 40);
-        Map.rect[10].saveRect(360, 720, 40, 40);
-        Map.rect[11].saveRect(440, 720, 40, 40);
-        Map.rect[12].saveRect(520, 720, 40, 40);
-        Map.rect[13].saveRect(600, 720, 40, 40);
-        Map.rect[14].saveRect(680, 720, 40, 40);
-        Map.rect[15].saveRect(760, 720, 40, 40);
-        for (let i = 0; i < 16; i++) {
-            Map.rect[i].saveFill("brown");
-            this.addChild(Map.rect[i]);
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                Map.rectLeft[i * 8 + j].saveRect(0 + j * 40, 480 + i * 40, 40, 40);
+            }
+        }
+
+        for (let i = 0; i < 8; i++) {
+            for (let j = 0; j < 8; j++) {
+                Map.rectRight[i * 8 + j].saveRect(680 + j * 40, 480 + i * 40, 40, 40);
+            }
+        }
+
+        for (let i = 0; i < 5; i++) {
+                Map.rectMid[i].saveRect(400 + i * 40, 400, 40, 40);
+        }
+
+        for (let i = 0; i < 64; i++) {
+            Map.rectLeft[i].saveFill("brown");
+            this.addChild(Map.rectLeft[i]);
+        }
+        for (let i = 0; i < 64; i++) {
+            Map.rectRight[i].saveFill("brown");
+            this.addChild(Map.rectRight[i]);
+        }
+        for (let i = 0; i < 5; i++) {
+            Map.rectMid[i].saveFill("brown");
+            this.addChild(Map.rectMid[i]);
+        }
+        for (let i = 0; i < 21; i++) {
+            Map.line[i].saveLine({x: 0, y: i * 40}, {x: 1000, y: i * 40});
+        }
+        for (let i = 0; i < 26; i++) {
+            Map.line[i + 21].saveLine({x: i * 40, y: 0}, {x: i * 40, y: 800});
         }
         return this;
     }
--- src/Shape.ts
+++ src/Shape.ts
@@ -1,5 +1,7 @@
 export default class Shape {
 
+    public static id: number;
+    public uuid: string;
     public type: string;
     public fillStyle: string;
     public strokeStyle: string;
@@ -11,33 +13,53 @@ export default class Shape {
     public startAngle: number;
     public endAngle: number;
 
+    constructor() {
+        Shape.id = 0;
+    }
+
     public saveRect(x: number, y: number, width: number, height: number) {
+        this.uuid = Date() + Shape.id;
         this.type = "rect";
         this.position = [{ x, y }];
         this.width = width;
         this.height = height;
+        Shape.id ++;
         return this;
     }
 
     public saveCircle(x: number, y: number, radius: number) {
+        this.uuid = Date() + Shape.id;
         this.type = "circle";
         this.position = [{ x, y }];
         this.radius = radius;
+        Shape.id ++;
         return this;
     }
 
     public saveArc(x: number, y: number, radius: number, startAngle: number, endAngle: number) {
+        this.uuid = Date() + Shape.id;
         this.type = "arc";
         this.position = [{ x, y }];
         this.radius = radius;
         this.startAngle = startAngle;
         this.endAngle = endAngle;
+        Shape.id ++;
         return this;
     }
 
     public saveTriangle(p1: {[key: string]: any}, p2: {[key: string]: any}, p3: {[key: string]: any}) {
+        this.uuid = Date() + Shape.id;
         this.type = "triangle";
         this.position = [p1, p2, p3];
+        Shape.id ++;
+        return this;
+    }
+
+    public saveLine(p1: {[key: string]: any}, p2: {[key: string]: any}) {
+        this.uuid = Date() + Shape.id;
+        this.type = "line";
+        this.position = [p1, p2];
+        Shape.id ++;
         return this;
     }
 

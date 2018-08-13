d3e2e06ff16234f5e046d66a99fd57cd53e6b4c6
gongrongyun <1716175849@qq.com>
2018-08-13 13:22:15 +0800

    Map 2000 1600

--- src/Map.ts
+++ src/Map.ts
@@ -13,58 +13,58 @@ export default class Map extends Container {
         Map.rectRight = [];
         Map.rectMid = [];
         Map.line = [];
-        for (let i = 0; i < 64; i++) {
+        for (let i = 0; i < 100; i++) {
             Map.rectLeft[i] = new Shape();
         }
-        for (let i = 0; i < 64; i++) {
+        for (let i = 0; i < 100; i++) {
             Map.rectRight[i] = new Shape();
         }
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 10; i++) {
             Map.rectMid[i] = new Shape();
         }
-        for (let i = 0; i < 47; i++) {
+        for (let i = 0; i < 92; i++) {
             Map.line[i] = new Shape();
         }
     }
 
     public createMap() {
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                Map.rectLeft[i * 8 + j].saveRect(0 + j * 40, 480 + i * 40, 40, 40);
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                Map.rectLeft[i * 10 + j].saveRect(0 + j * 40, 1200 + i * 40, 40, 40);
             }
         }
 
-        for (let i = 0; i < 8; i++) {
-            for (let j = 0; j < 8; j++) {
-                Map.rectRight[i * 8 + j].saveRect(680 + j * 40, 480 + i * 40, 40, 40);
+        for (let i = 0; i < 10; i++) {
+            for (let j = 0; j < 10; j++) {
+                Map.rectRight[i * 10 + j].saveRect(1600 + j * 40, 1200 + i * 40, 40, 40);
             }
         }
 
-        for (let i = 0; i < 5; i++) {
-                Map.rectMid[i].saveRect(400 + i * 40, 400, 40, 40);
+        for (let i = 0; i < 10; i++) {
+                Map.rectMid[i].saveRect(800 + i * 40, 1080, 40, 40);
         }
 
-        for (let i = 0; i < 64; i++) {
+        for (let i = 0; i < 100; i++) {
             Map.rectLeft[i].saveFill("#8B0000");
             this.addChild(Map.rectLeft[i]);
         }
-        for (let i = 0; i < 64; i++) {
+        for (let i = 0; i < 100; i++) {
             Map.rectRight[i].saveFill("#8B0000");
             this.addChild(Map.rectRight[i]);
         }
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < 10; i++) {
             Map.rectMid[i].saveFill("#8B0000");
             this.addChild(Map.rectMid[i]);
         }
-        for (let i = 0; i < 21; i++) {
-            Map.line[i].saveLine({x: 0, y: i * 40}, {x: 1000, y: i * 40});
+        for (let i = 0; i < 41; i++) {
+            Map.line[i].saveLine({x: 0, y: i * 40}, {x: 2000, y: i * 40});
             Map.line[i].saveStroke("rgba(0, 0, 0, 0.1)", 2);
             this.addChild(Map.line[i]);
         }
-        for (let i = 0; i < 26; i++) {
-            Map.line[i + 21].saveLine({x: i * 40, y: 0}, {x: i * 40, y: 800});
-            Map.line[i + 21].saveStroke("rgba(0, 0, 0, 0.1)", 2);
-            this.addChild(Map.line[i + 21]);
+        for (let i = 0; i < 51; i++) {
+            Map.line[i + 41].saveLine({x: i * 40, y: 0}, {x: i * 40, y: 1600});
+            Map.line[i + 41].saveStroke("rgba(0, 0, 0, 0.1)", 2);
+            this.addChild(Map.line[i + 41]);
         }
         return this;
     }

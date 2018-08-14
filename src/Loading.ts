9024f789b12477aea3ee21a9fdeb5d9ada406c22
gongrongyun <1716175849@qq.com>
2018-08-14 15:51:24 +0800

    Role no move

--- src/Loading.ts
+++ src/Loading.ts
@@ -1,8 +1,10 @@
 import Main from "./Main";
 import Menu from "./Menu";
+import Role from "./Role";
 
 const start = () => {
     Menu.createMenuMap();
+    Role.create(7, 28);
     Menu.createGoods();
     const main = new Main();
     main.createScene();
